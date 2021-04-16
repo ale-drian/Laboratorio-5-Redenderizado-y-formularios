abfefb83ab5cddf177c6825e3b4a076d021bceeb
unknown <alejandraadriantejada@gmail.com>
2021-04-15 22:06:22 -0500

    agrega y lista

--- guia_telefonica/src/App.js
+++ guia_telefonica/src/App.js
@@ -6,19 +6,32 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleChangeName = (event) => {
+    setNewName(event.target.value);
+  }
+  
+  const handleSubmitAddPerson = (event) => {
+    event.preventDefault();
+    let newPerson = {
+      name: newName
+    }
+    setPersons([...persons, newPerson]);
+    event.target.reset(); //resetear
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmitAddPerson}>
         <div>
-          name: <input />
+          name: <input onChange={handleChangeName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p>{person.name}</p>)}
     </div>
   )
 }
